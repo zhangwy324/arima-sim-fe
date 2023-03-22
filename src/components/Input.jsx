94b86736b118ea8aac4256c87f0da6d78e049b00
Zhang Weiye <zhangwy@ad.unc.edu>
2023-03-22 18:18:54 -0400

    created branch, added comment TODO

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -121,7 +121,7 @@ export default function Input({ setPlotData, setError }) {
       headers: myHeaders,
       body: body,
     };
-    fetch("http://127.0.0.1:4000/sarimaApi", requestOptions)
+    fetch("http://127.0.0.1:4000/sarimaApi", requestOptions) // TODO: Change to deployed server url
       .then((res) => {
         return res.json();
       })
