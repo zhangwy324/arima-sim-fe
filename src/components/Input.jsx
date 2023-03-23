bf9a745e56de97b54e9a36750c97e66692785d40
Zhang Weiye <zhangwy@ad.unc.edu>
2023-03-23 17:02:47 -0400

    fixed Railway server Url

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -129,7 +129,7 @@ export default function Input({ setPlotData, setError, setErrorObj, setIsFetched
       serverURL = "http://127.0.0.1:4000/sarima";
     } else if (import.meta.env.MODE === "production") {
       console.log("production mode");
-      serverURL = "https://arima-sim-be-production.up.railway.app/";
+      serverURL = "https://arima-sim-be-production.up.railway.app/sarima";
     }
     console.log(serverURL);
     fetch(serverURL, requestOptions)
